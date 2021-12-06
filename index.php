f50101901d629bf26cb9da19f5d6b36fd83d4ff0
IidaPeltonen <n0peii00@students.oamk.fi>
2021-12-07 00:32:49 +0200

    valmis, kommentoitu

--- index.php
+++ index.php
@@ -1,23 +1,13 @@
 <?php
 
-require_once('db.php');
-
-function selectAsJson(object $db,string $sql): void {
-    $query = $db->query($sql);
-    $results = $query->fetchAll(PDO::FETCH_ASSOC);
-    header('HTTP/1.1 200 OK');
-    echo json_encode($results);
-}
-
-function returnError(PDOException $pdoex): void {
-    header('HTTP/1.1 500 Internal Server Error');
-    $error = array('error' => $pdoex->getMessage());
-    echo json_encode($error);
-    exit;
-}
+//otetaan funkitiot käyttöön
+require('functions.php');
 
 try {
+//avataan kanta
 $dbcon = createDbConnection();
+//muodotetaan sql-lause, haetaan otsikot, vuosi, kesto, arvostelut. Etsitään elokuvat, joissa roolinimenä Bean 
+// järjestetään nousevaan laskevaan järjestykseen arvosanan mukaan
 selectAsJson($dbcon, 'SELECT titles.title_id, primary_title, start_year, average_rating 
     FROM titles, had_role, title_ratings
     WHERE titles.title_id = had_role.title_id AND role_ LIKE "%Mr. Bean%" AND titles.title_id =title_ratings.title_id
