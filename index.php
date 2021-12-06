b0385166023a85878d9ace386e2a1779f8bedaac
IidaPeltonen <n0peii00@students.oamk.fi>
2021-12-06 23:37:37 +0200

    ok

--- index.php
+++ index.php
@@ -1,18 +1,33 @@
 <?php
 
-    require_once('db.php');
-    $dbcon = createDbConnection();
-    $sql = "SELECT title FROM aliases WHERE (region = 'FI') GROUP BY title_id ORDER BY title LIMIT 10;";
-    //$sql = "CALL GetAliasesByCountry('FI');";
-    $prepare = $dbcon->prepare($sql);   //valmistellaan
-        $prepare->execute();  //kysely tietokantaan
-        $rows = $prepare->fetchAll(); //haetaan tulokset (voitaisiin hakea myös eka rivi fetch ja tarkistus)
-        $html = '<h1>Top 10 Finnish titles</h1>';
-        $html .= '<ul>';
-        //Käydään rivit läpi (max yksi rivi tässä tapauksessa) 
-        foreach($rows as $row){
-            $html .= '<li>' . $row["title"] . '</li>';  
-        }
-        $html .= '</ul>';
+require_once('db.php');
 
-        echo $html;
+function selectAsJson(object $db,string $sql): void {
+    $query = $db->query($sql);
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    header('HTTP/1.1 200 OK');
+    echo json_encode($results);
+}
+
+function returnError(PDOException $pdoex): void {
+    header('HTTP/1.1 500 Internal Server Error');
+    $error = array('error' => $pdoex->getMessage());
+    echo json_encode($error);
+    exit;
+}
+
+try {
+$dbcon = createDbConnection();
+selectAsJson($dbcon, 'SELECT titles.title_id, primary_title, start_year, average_rating 
+    FROM titles, had_role, title_ratings
+    WHERE titles.title_id = had_role.title_id AND role_ LIKE "%Mr. Bean%" AND titles.title_id =title_ratings.title_id
+    group BY titles.title_id 
+    order by average_rating DESC
+    LIMIT 10');
+  }  catch (PDOException $pdoex) {
+        returnError($pdoex);
+    }
+
+    
+
+
